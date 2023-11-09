ed5acb0ba851b4beeb288d27d98816b45cceb95e
Nobuyuki Akama <nakama@microsoft.com>
2023-11-09 14:19:28 +0900

    null 処理対応

--- AzRefArc.AspNetBlazorUnited.Client/Pages/Samples/InteractiveAutoListAuthorsServiceClientImpl.cs
+++ AzRefArc.AspNetBlazorUnited.Client/Pages/Samples/InteractiveAutoListAuthorsServiceClientImpl.cs
@@ -10,13 +10,13 @@ namespace AzRefArc.AspNetBlazorUnited.Client.Pages.Samples
 
         public InteractiveAutoListAuthorsServiceClientImpl(HttpClient httpClient, ILogger<InteractiveAutoListAuthorsServiceClientImpl> logger)
         {
-            this.httpClient = httpClient;
-            this.logger = logger;
+            this.httpClient = httpClient ?? throw new InvalidOperationException("httpClient");
+            this.logger = logger ?? throw new InvalidOperationException("logger"); ;
         }
 
         public async Task<List<Author>> GetAuthorsAsync()
         {
-            return await httpClient.GetFromJsonAsync<List<Author>>("/api/Samples/InteractiveAutoListAuthors/GetAuthors")!;
+            return (await httpClient.GetFromJsonAsync<List<Author>>("/api/Samples/InteractiveAutoListAuthors/GetAuthors"))!;
         }
     }
 }
