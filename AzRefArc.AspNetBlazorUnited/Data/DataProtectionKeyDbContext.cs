fab25b1eb2daa609ba509aa6d206d65e988ba78e
Nobuyuki Akama <nakama@microsoft.com>
2024-07-01 22:39:25 +0900

    本番環境でキーをそろえるための機能を追加

---
+++ AzRefArc.AspNetBlazorUnited/Data/DataProtectionKeyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzRefArc.AspNetBlazorUnited.Data
+{
+    public class DataProtectionKeyDbContext: DbContext, IDataProtectionKeyContext
+    {
+        public DataProtectionKeyDbContext(DbContextOptions<DataProtectionKeyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; } = null!;
+    }
+}
--- AzRefArc.AspNetBlazorUnited/Program.cs
+++ AzRefArc.AspNetBlazorUnited/Program.cs
@@ -4,6 +4,8 @@ using AzRefArc.AspNetBlazorUnited.Components.Pages.Samples;
 using AzRefArc.AspNetBlazorUnited.Data;
 using Microsoft.EntityFrameworkCore;
 using AzRefArc.AspNetBlazorUnited.Client.Pages.Samples;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace AzRefArc.AspNetBlazorUnited
 {
@@ -47,6 +49,36 @@ namespace AzRefArc.AspNetBlazorUnited
 
             // アプリサービスの追加(サーバ用)
             builder.Services.AddScoped(typeof(IInteractiveAutoListAuthorsService), typeof(InteractiveAutoListAuthorsServiceServerImpl));
+
+            // 本番環境用の場合、サーバ間で鍵をそろえる必要がある
+            // 鍵はファイル共有や Keyvault に格納できるが、ここでは追加リソースを構成する手間を避けるために DB に保存してしまうことにする
+            // 本番環境用の DB に対して以下のクエリを実行し、テーブルを追加で作成しておく
+            // CREATE TABLE[dbo].[DataProtectionKeys] ( [ID][int] IDENTITY(1, 1) NOT NULL PRIMARY KEY, [FriendlyName] [varchar] (64) NULL, [Xml][text] NULL)
+            // ※ 開発環境ではこのテーブルが不要になるように、ここでは同一 DB に対して二つの DbContext を使うように実装している
+            // この機能を使う場合には、DataProtection:UseSharedKeyOnDatabase 設定を true にすること
+            //"DataProtection": {
+            //    "UseSharedKeyOnDatabase":  true
+            //}
+            string? useSharedKeyOnDatabase = builder.Configuration["DataProtection:UseSharedKeyOnDatabase"];
+            if (string.IsNullOrEmpty(useSharedKeyOnDatabase) == false && bool.Parse(useSharedKeyOnDatabase))
+            {
+                builder.Services.AddDbContextFactory<DataProtectionKeyDbContext>(opt =>
+                {
+                    // DbContext 構成設定
+                    // https://docs.microsoft.com/ja-jp/ef/core/dbcontext-configuration/#other-dbcontext-configuration
+                    if (builder.Environment.IsDevelopment())
+                    {
+                        opt = opt.EnableSensitiveDataLogging().EnableDetailedErrors();
+                    }
+                    opt.UseSqlServer(
+                        builder.Configuration.GetConnectionString("PubsDbContext"),
+                        providerOptions =>
+                        {
+                            providerOptions.EnableRetryOnFailure();
+                        });
+                });
+                builder.Services.AddDataProtection().PersistKeysToDbContext<DataProtectionKeyDbContext>();
+            }
 
             var app = builder.Build();
 
